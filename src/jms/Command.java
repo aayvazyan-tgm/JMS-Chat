8cf153cc470e777fa72c5f4aec2973352e92efe6
aayvazyan-tgm <ariay@live.at>
2014-02-12 19:26:30 +0100

    Javadoc added

--- src/jms/Command.java
+++ src/jms/Command.java
@@ -1,5 +1,14 @@
 package jms;
 
+/**
+ * @author Wolfgang Mair
+ * @version 12.02.2014
+ * The Enum Command defines aviable commands.
+ */
 public enum Command {
-	_MAIL, _MAILBOX
+	
+	/** The  mail command. */
+	_MAIL, 
+ /** The  mailbox command. */
+ _MAILBOX
 }
